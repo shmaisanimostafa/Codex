ba733c15d5a7568872226d87a19870bd026bb5d8
Mostafa Shmaisani <shmaisanimostafa@gmail.com>
2023-02-21 16:01:26 +0200

    Change Server

--- client/script.js
+++ client/script.js
@@ -92,7 +92,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   //Fetch data from the server (Bot's response)
-  const response = await fetch("http://localhost:5000/", {
+  const response = await fetch("https://codex-y0dw.onrender.com/", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
